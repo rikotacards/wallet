39dacdefb50b0a29e268df2f54a9528667109b3b
max hsu <maxhsu1@gmail.com>
2024-05-08 12:13:35 +0700

    add vite dep.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  optimizeDeps: {
+    include: ['@emotion/styled'],
+  },
+});
+
+//https://medium.com/@fael-atom/struggling-with-vite-and-mui-42f3f5e0658d
